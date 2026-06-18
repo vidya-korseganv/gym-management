1f2ef3783f1f5f25b5a3e7b435853dfaa1887c93
vidya-korseganv <vidyakorseganv24@gmail.com>
2026-06-18 11:42:37 +0530

    Fix gym entity property initialization

--- src/gym/entities/gym.entity.ts
+++ src/gym/entities/gym.entity.ts
@@ -1,8 +1,8 @@
 export class Gym {
-  id: number;
-  name: string;
-  location: string;
-  monthlyFee: number;
-  isOpen: boolean;
-  createdAt: Date;
+  id!: number;
+  name!: string;
+  location!: string;
+  monthlyFee!: number;
+  isOpen!: boolean;
+  createdAt!: Date;
 }
